4437d34fdba767f0a2b753ed2a7cbd583c95bdea
Pawel Moll <pawel.moll@arm.com>
2012-09-18 13:46:36 +0100

    ARM: vexpress: Reset driver
    This is a simple driver providing platform restart and power off
    functions using VE config infrastructure.
    
    By writing to the "active" attribute of the reboot or reset device,
    user can decide what if the platform is supposed to execute full power
    cycle (reboot, default) or simply assert system level reset signal.
    
    Signed-off-by: Pawel Moll <pawel.moll@arm.com>

--- include/linux/vexpress.h
+++ include/linux/vexpress.h
@@ -107,4 +107,7 @@ unsigned __vexpress_get_site(struct device *dev, struct device_node *node);
 void vexpress_sysreg_early_init(void __iomem *base);
 void vexpress_sysreg_of_early_init(void);
 
+void vexpress_power_off(void);
+void vexpress_restart(char str, const char *cmd);
+
 #endif
